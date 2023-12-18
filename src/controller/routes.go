25839fa9e59618eb79cbefaae4dc05521a0ed176
sungyo4869 <hchengjian1@gmail.com>
2023-12-18 16:31:37 +0900

    [update] ミドルウェアを増やした

--- src/controller/routes.go
+++ src/controller/routes.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"src/middleware"
 )
 
 func GetRouter() *gin.Engine {
@@ -13,12 +14,13 @@ func GetRouter() *gin.Engine {
 	r.GET("/logout", logout)
 	//サインアップ
 	r.POST("/sign-up", signUp)
+	
 	// ランキングの取得
-	r.GET("/ranking", showRanking)
+	r.GET("/ranking", middleware.AuthMiddleware(), getRanking)
 	// 首都を引数としてスコアを返す
-	r.GET("/score", getScore)
+	r.GET("/score", middleware.AuthMiddleware(), getScore)
 	// ハイスコアかの判定、ハイスコアならランキングを更新して返す、じゃないならそのままの値を返す
-	r.GET("/result", getResult)
+	r.GET("/result", middleware.AuthMiddleware(), getResult)
 
 	return r
 }
