4d92dc04825aba48f4a532462f97d2c6961e5810
sungyo4869 <hchengjian1@gmail.com>
2023-12-18 16:32:00 +0900

    [update] コントローラーの実装

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,62 +1,163 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
-	
 	"src/model"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
 func login(c *gin.Context) {
-	// model.searchUser()
-	// 存在してたらmodel.createToken()
+	var newUser model.User
+
+	c.ShouldBindJSON(&newUser)
+
+	// ユーザーがいるかどうか探す
+	userID, err := model.GetUserID(&newUser)
+
+	if err != nil{
+		c.JSON(401, gin.H{"error":"ユーザーが見つかりません"})
+		return
+	}
+
+	tokenString, err := model.GenerateToken(userID)
+	if err != nil{
+		c.JSON(401, gin.H{"error":"トークンの作成に失敗しました"})
+	}
+
+	c.JSON(200, gin.H{"token":tokenString})
 }
 
 func logout(c *gin.Context) {
-	// DBに登録してあるトークンを削除
+	c.JSON(200, gin.H{"message":"ok"})
 }
 
 func signUp(c *gin.Context) {
-	// model.searchUserで同じ名前のアカウントがないか確認
-
 	var newUser model.User
 
-	// リクエストボディからユーザー情報を取得
+	// リクエストボディからユーザーが入力した情報を取得
 	if err := c.ShouldBindJSON(&newUser); err != nil {
 		c.JSON(400, gin.H{"error": "無効なリクエスト"})
+		return
+	}
+
+	// 同じ名前のユーザーがいないか
+	_, result := model.GetUserID(&newUser)
+
+	if result == nil{
+		c.JSON(200, gin.H{"error":"ユーザーネームはすでに使用されています"})
 		return
 	}
 
 	// パスワードをハッシュ化する処理
 
 	// modelパッケージのCreateUserを呼び出す
-	result := model.CreateUser(&newUser)
+	result = model.CreateUser(&newUser)
 
 	if result != nil{
 		c.JSON(500, gin.H{"error": "ユーザーの作成に失敗しました"})
+		return
 	}
 
 	c.JSON(200, gin.H{"message": "ユーザーが正常に登録されました"})
-	// トークンを生成
 }
 
-func showRanking(c *gin.Context){
-	// トークンをヘッダーから取得
-	// model.SearchToken()
-	// trueならランキングを返す
+func getRanking(c *gin.Context){
+
+	var ranking model.Ranking
+
+	scores, err := model.GetRanking()
+
+	if err != nil{
+		c.JSON(400, gin.H{"error":"ランキングの取得に失敗しました"})
+		return
+	}
+
+	for _, userScore := range scores{
+		username, result := model.GetUserName(userScore.UserID)
+
+		if result != nil{
+			c.JSON(400, gin.H{"error":"ランキングの取得に失敗しましたあ"})
+			return
+		}
+
+		score := model.UserScore{
+			UserName: username,
+			Score:    userScore.Score,
+		}
+
+		ranking.Scores = append(ranking.Scores, score)
+	}
+
+	c.JSON(200, ranking)
 }
 
+// scoreを返す、ハイスコアが更新されたらtrue
 func getResult(c *gin.Context){
-	// トークンをヘッダーから取得
-	// model.SearchToken()
-	// model.GetHighScore()
-	// 比較
-	// ハイスコアならmodel.updateHighScore()
-	// じゃないならそのままの値を返す
+
+	// 投げられてくる形を定義
+	type jsonScore struct{
+		Score int `json:"score"`
+	}
+
+	var userScore jsonScore
+
+	if err := c.ShouldBindJSON(&userScore); err != nil {
+		c.JSON(400, gin.H{"error": "無効なリクエスト"})
+		return
+	}
+
+	userID := getUserIDforHeader(c)
+
+	dbScore,err := model.GetHighScore(userID)
+
+	if err != nil{
+		c.JSON(400, gin.H{"error":"ハイスコアの取得に失敗しました"})
+		return
+	}
+	
+	// ハイスコアが更新されたか
+	if dbScore < userScore.Score{
+		newScore := model.Scores{
+			Score: userScore.Score,
+			UserID: userID,
+		}
+		
+		model.UpdateHighScore(&newScore)
+
+		c.JSON(200, gin.H{"score":newScore.Score, "isUpdateHighScore":true})
+		return
+	}
+
+	c.JSON(200, gin.H{"score":userScore.Score, "isUpdateHighScore":false})	
 }
 
 func getScore(c *gin.Context){
-	// トークンをヘッダーから取得
-	// model.SearchToken()
-	// model.getScore()
-	// 返ってきた値を足し算する
+
+	type jsonCity struct {
+		Cities []string `json:"cities"`
+	}
+
+	var cities jsonCity
+
+	if err := c.ShouldBindJSON(&cities); err != nil {
+		c.JSON(400, gin.H{"error": "無効なリクエスト"})
+		return
+	}
+	
+	result, err := model.GetScore(cities.Cities)
+
+	if err != nil{
+		c.JSON(400, gin.H{"error": "スコアの取得に失敗しました"})
+	}
+
+	c.JSON(200, gin.H{"score":result})
+}
+
+func getUserIDforHeader(c *gin.Context) int {
+
+	claims := c.MustGet("claims").(jwt.MapClaims)
+	userID := int(claims["user_id"].(float64)) // ユーザーIDの取得
+
+	return userID
 }
