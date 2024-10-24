e3f8b10605a8e4fe9391ba1f42538317895397aa
sungyo4869 <hchengjian1@gmail.com>
2024-10-25 00:21:46 +0900

    動くようにした

--- src/controller/routes.go
+++ src/controller/routes.go
@@ -9,7 +9,7 @@ func GetRouter() *gin.Engine {
 	r := gin.Default()
 
 	// ログイン
-	r.GET("/login", login)
+	r.POST("/login", login)
 	// ログアウト
 	r.GET("/logout", logout)
 	//サインアップ
@@ -18,9 +18,9 @@ func GetRouter() *gin.Engine {
 	// ランキングの取得
 	r.GET("/ranking", middleware.AuthMiddleware(), getRanking)
 	// 首都を引数としてスコアを返す
-	r.GET("/score", middleware.AuthMiddleware(), getScore)
+	r.POST("/score", middleware.AuthMiddleware(), getScore)
 	// ハイスコアかの判定、ハイスコアならランキングを更新して返す、じゃないならそのままの値を返す
-	r.GET("/result", middleware.AuthMiddleware(), getResult)
+	r.POST("/result", middleware.AuthMiddleware(), getResult)
 
 	return r
 }
--- src/sh/start_app.sh
+++ src/sh/start_app.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# 改行コードをLFにする
+
 until mysqladmin ping -h mysql -P 3306 -u root -ppass; do
   echo 'waiting for mysqld to be connectable...'
   sleep 2
